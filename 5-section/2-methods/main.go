657f6216512d8f4e59a28eaadc4269507c820b0c
JamestarKurbah <jamestarkurbah@gmail.com>
2026-05-30 21:31:37 +0530

    added methods with value receiver and pointer receiver

---
+++ 5-section/2-methods/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Employee struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Position  string
+	Salary    int
+	IsActive  bool
+	JoinedAt  time.Time
+}
+
+func NewEmployee(id int, firstname, lastName, position string, salary int, isActive bool) Employee {
+	return Employee{
+		ID:        id,
+		FirstName: firstname,
+		LastName:  lastName,
+		Position:  position,
+		Salary:    salary,
+		IsActive:  isActive,
+		JoinedAt:  time.Now(),
+	}
+}
+
+// FullName pointer receiver
+func (e *Employee) FullName() string {
+	if e == nil {
+		return ""
+	}
+	return e.FirstName + " " + e.LastName
+}
+
+// // Deactivate value receiver
+// func (e Employee) Deactivate() { // copy of data
+// 	e.IsActive = false
+// }
+
+// Deactivate pointer receiver
+func (e *Employee) Deactivate() { // copy of data
+	e.IsActive = false
+}
+
+func (e *Employee) SetJoinedAt(t time.Time) {
+	e.JoinedAt = t
+}
+
+func main() {
+	jane := Employee{
+		ID:        1,
+		FirstName: "Jane",
+		LastName:  "Doe",
+		Position:  "Night",
+		Salary:    10000,
+		IsActive:  true,
+		//JoinedAt:  time.Now(),
+	}
+	fmt.Println(jane)
+	jane.Deactivate()
+	fmt.Println(jane)
+	jane.SetJoinedAt(time.Now().Add(100000000 * time.Minute))
+	fmt.Println(jane)
+	// emptying
+	joe := &Employee{}
+	joe = nil
+	joe.FullName()
+}
