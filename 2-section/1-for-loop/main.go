306925e6840cc3ce66ed3c9a163aec570d2ad4a8
JamestarKurbah <jamestarkurbah@gmail.com>
2026-05-30 12:29:38 +0530

    for loop example

---
+++ 2-section/1-for-loop/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+// in go for is the only way to loop
+
+func main() {
+	// C-style loop
+	for i := 1; i <= 10; i++ {
+		//fmt.Println(i)
+	}
+	// while-style
+	k := 3
+	for k > 0 {
+		fmt.Println(k)
+		k--
+	}
+	fmt.Println("------------------infinite loop------------------------")
+	counter := 0
+	for {
+		fmt.Println("counter ", counter)
+		counter++
+		if counter >= 5 {
+			break
+		}
+	}
+	fmt.Println("---------------- skipping --------------")
+
+	for i := 1; i <= 10; i++ {
+		if i%2 == 0 {
+			continue
+		}
+		fmt.Println(i)
+	}
+
+	fmt.Println("----------- array --------------")
+	items := []string{"Go", "Program", "Java"}
+
+	for _, value := range items {
+		fmt.Println(value)
+	}
+}
